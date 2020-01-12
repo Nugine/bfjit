c177c19fcaf2fc6f031e48436867f50ad9bdd775
Nugine <nugine@foxmail.com>
2020-01-12 17:48:57 +0800

    book: chapter 4

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,42 @@
+#![feature(proc_macro_hygiene)]
+
 mod bfir;
 
+use dynasm::dynasm;
+use dynasmrt::{DynasmApi, DynasmLabelApi};
+
+use std::io::{stdout, Write};
+
+unsafe extern "sysv64" fn print(buf: *const u8, len: u64) -> u8 {
+    let buf = std::slice::from_raw_parts(buf, len as usize);
+    stdout().write_all(buf).is_err() as u8
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut ops = dynasmrt::x64::Assembler::new().unwrap();
+    let string = b"Hello, JIT!\n";
+
+    dynasm!(ops
+        ; .arch x64
+        ; ->hello:
+        ; .bytes string
+    );
+
+    let hello = ops.offset();
+    dynasm!(ops
+        ; lea rdi, [->hello]
+        ; mov rsi, QWORD string.len() as _
+        ; mov rax, QWORD print as _
+        ; call rax
+        ; ret
+    );
+
+    let buf = ops.finalize().unwrap();
+
+    let hello_fn: unsafe extern "sysv64" fn() -> u8 =
+        unsafe { std::mem::transmute(buf.ptr(hello)) };
+
+    let ret = unsafe { hello_fn() };
+
+    assert_eq!(ret, 0);
 }
