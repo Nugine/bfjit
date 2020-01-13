f8f00e58c72b95a3b6237782dbdefa31f3b97189
Nugine <nugine@foxmail.com>
2020-01-13 20:11:52 +0800

    bfjit: impl main

--- src/main.rs
+++ src/main.rs
@@ -5,20 +5,38 @@ mod bfjit;
 mod error;
 
 use crate::bfjit::BfVM;
-use std::io::{stdin, stdout, Read, Write};
-use std::path::Path;
+
+use std::io::{stdin, stdout};
+use std::path::PathBuf;
+
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+struct Opt {
+    #[structopt(name = "FILE")]
+    file_path: PathBuf,
+
+    #[structopt(short = "o", long = "optimize", help = "Optimize code")]
+    optimize: bool,
+}
 
 fn main() {
+    let opt = Opt::from_args();
+
     let stdin = stdin();
     let stdout = stdout();
-    let path = std::env::args().nth(1).unwrap();
-    let mut vm = BfVM::new(
-        Path::new(&path),
+
+    let ret = BfVM::new(
+        &opt.file_path,
         Box::new(stdin.lock()),
         Box::new(stdout.lock()),
-        true
+        opt.optimize,
     )
-    .unwrap();
+    .and_then(|mut vm| vm.run());
 
-    vm.run().unwrap();
+    if let Err(e) = &ret {
+        eprintln!("bfjit: {}", e);
+    }
+
+    std::process::exit(ret.is_err() as i32)
 }
